b3adfb052b4c7f1c2218e3a61b5796f23ece653e
AsrH61 <20080617@student.hut.edu.vn>
2019-12-14 00:02:39 +0700

    abc

--- js/index.js
+++ js/index.js
@@ -13,3 +13,15 @@ function menu(){
         element.className = classes.join(" "); 
     }
 }
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
