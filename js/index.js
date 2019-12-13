670cb2db1e71122b2b36f1f133f8d6a397dfab88
QD <20080617@student.hut.edu.vn>
2019-12-13 15:26:57 +0700

    abc

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
 function menu(){
-    let menu = document.getElementsByClassName('mainnav-collapse');
-    let open = document.querySelector(".mainnav-collapse");
-    let style1 = getComputedStyle(open);
-    let i = 0, n = menu.length;
-    for(i = 0; i < n; i++){
-        let element = menu[i];
-        console.log("open",menu);
-        if(style1.display == "none" ){
-            console.log("ok")
-            element.style.display = "block";
-        }else{
-            element.style.display = "none";
-        }
-        
+    var element = document.getElementById("nav_right");
+    if (element.classList) { 
+        element.classList.toggle("block");
+    } else {
+        var classes = element.className.split(" ");
+        var i = classes.indexOf("block");
+    
+        if (i >= 0) 
+        classes.splice(i, 1);
+        else 
+        classes.push("block");
+        element.className = classes.join(" "); 
     }
 }
