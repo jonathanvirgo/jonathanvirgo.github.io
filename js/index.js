ce3d11ee26787f2ae77833f4b65b5cd0e8b021c9
AsrH61 <20080617@student.hut.edu.vn>
2019-12-15 09:36:04 +0700

    add swiper

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
 function menu(){
-    var element = document.getElementById("nav_right");
-    if (element.classList) { 
-        element.classList.toggle("block");
-    } else {
-        var classes = element.className.split(" ");
-        var i = classes.indexOf("block");
-    
-        if (i >= 0) 
-        classes.splice(i, 1);
-        else 
-        classes.push("block");
-        element.className = classes.join(" "); 
-    }
+  console.log("ABC")
+  var element = document.getElementById("nav_right");
+  if (element.classList) { 
+      element.classList.toggle("block");
+  } else {
+      var classes = element.className.split(" ");
+      var i = classes.indexOf("block");
+  
+      if (i >= 0) 
+      classes.splice(i, 1);
+      else 
+      classes.push("block");
+      element.className = classes.join(" "); 
+  }
 }
+$(document).ready(function(){
+var swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+});
